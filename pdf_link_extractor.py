54e6e69f12c776aa85cdcadf039eae2a3d8e4165
Alfonzo CS <70090840+AlfonzCS@users.noreply.github.com>
2020-09-01 20:37:04 -0400

    Add files via upload

---
+++ pdf_link_extractor.py
@@ -0,0 +1,16 @@
+#pdf_table_extractor.py
+import pikepdf # pip3 install pikepdf
+
+file = "1810.04805.pdf"
+# file = "1710.05006.pdf"
+pdf_file = pikepdf.Pdf.open(file)
+urls = []
+# iterate over PDF pages
+for page in pdf_file.pages:
+    for annots in page.get("/Annots"):
+        uri = annots.get("/A").get("/URI")
+        if uri is not None:
+            print("[+] URL Found:", uri)
+            urls.append(uri)
+
+print("[*] Total URLs extracted:", len(urls))
